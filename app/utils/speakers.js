0e5a5588f3bcede8d1cbd15faa6d83147b85f07b
Tracy Lee <tracy@wingman.events>
2015-11-27 20:21:29 -0800

    changes

--- app/utils/speakers.js
+++ app/utils/speakers.js
@@ -1,71 +1,61 @@
 export default [{
 	cardTitle: 'Ben Lesh',
-	title: 'Partner',
+	title: 'Senior UI Engineer at Netflix',
 	image: 'img/speakers/ben-lesh.jpg',
-	cardContent: 'Jarrod Overson',
 	current: true
 },
 {
 	cardTitle: 'Ariya Hidayat',
-	title: 'Partner',
+	title: 'Creator PhantomJS, Esprima, VP of Engineering at Shape Security',
 	image: 'img/speakers/ariya-hidayat.jpg',
-	cardContent: 'Jarrod Overson',
 	current: true
 },
 {
 	cardTitle: 'Stefan Penner',
-	title: 'Partner',
+	title: 'EmberJS Core Team, Ember-CLI, TC39, Hacker @Yahoo',
 	image: 'img/speakers/stefan-penner.jpeg',
-	cardContent: 'Jarrod Overson',
 	current: true
 },
 {
 	cardTitle: 'Jafar Husain',
-	title: 'Partner',
+	title: 'Cross-Team Lead at Netflix and TC39 Member',
 	image: 'img/speakers/jafar-husain.jpg',
-	cardContent: 'Jarrod Overson',
 	current: true
 },
 {
 	cardTitle: 'Jay Phelps',
-	title: 'Partner',
+	title: 'Senior Front End Engineer at Netflix',
 	image: 'img/speakers/jay-phelps.jpeg',
-	cardContent: 'Jarrod Overson',
 	current: true
 },
 {
 	cardTitle: 'Godfrey Chan',
-	title: 'Partner',
+	title: 'Rails Core Team, EmberJS at Tilde',
 	image: 'img/speakers/godfrey-chan.jpg',
-	cardContent: 'Jarrod Overson',
 	current: true
 },
 {
 	cardTitle: 'Nathan Hammond',
-	title: 'Partner',
+	title: 'Principal Web Developer at LinkedIn',
 	image: 'img/speakers/nathan-hammond.jpg',
-	cardContent: 'Jarrod Overson',
 	current: true
 },
 {
 	cardTitle: 'Jim Sproch',
-	title: 'Partner',
+	title: 'ReactJS Core Team, Engineer at Facebook',
 	image: 'img/speakers/jim-sproch.jpg',
-	cardContent: 'Jarrod Overson',
 	current: true
 },
 {
 	cardTitle: 'Jarrod Overson',
-	title: 'Partner',
+	title: 'Co-Author of Developing Web Components, Creator Plato, RCL, Preprocess, Engineer at Shape Security',
 	image: 'img/speakers/jarrod-overson.jpg',
-	cardContent: 'Jarrod Overson',
 	current: true
 },
 {
 	cardTitle: 'Mike North',
-	title: 'Partner',
+	title: 'CTO of Levanto Financial',
 	image: 'img/speakers/mike-north.jpg',
-	cardContent: 'Jarrod Overson',
 	current: true
 }];
 
