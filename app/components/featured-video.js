3aef74a7d0815fa31f21adbb4981e8bf1a992256
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2016-01-23 22:43:53 -0500

    Initially load youtube images instead of videos because slow.

--- app/components/featured-video.js
+++ app/components/featured-video.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  imageShowing: Ember.computed(function() {
+    return true;
+  }),
+
+  actions: {
+    showVideo() {
+      this.set('imageShowing', false);
+    }
+  }
 });
