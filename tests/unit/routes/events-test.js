363dc9db336f978cba47b5e7bc2749add2f8c34f
Tracy Lee <tracy@wingman.events>
2016-03-14 20:50:07 -0400

    add tos and privacy policy

--- tests/unit/routes/events-test.js
+++ tests/unit/routes/events-test.js
@@ -1,11 +1,11 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:beginners', 'Unit | Route | beginners', {
+moduleFor('route:events', 'Unit | Route | events', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
 test('it exists', function(assert) {
-  var route = this.subject();
+  let route = this.subject();
   assert.ok(route);
 });
