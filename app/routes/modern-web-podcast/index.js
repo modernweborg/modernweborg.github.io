92b4fa726c83b42c41e1f4ba8cdd96d81bf88a55
Ben Lesh <blesh@netflix.com>
2016-07-18 16:38:58 -0700

    refactor(podcast index): change layout of podcasts
    - descriptions will now collapse
    - titles are now laid over top of the image
    - NOTE: index page no longer uses the md-card-panel component as it was inefficient.
    - NOTE: ifrome-panel is no longer used in the index, the podcast detail screen might be best off refactored at some point just to have its own template and logic
    
    <3

--- app/routes/modern-web-podcast/index.js
+++ app/routes/modern-web-podcast/index.js
@@ -1,7 +1,30 @@
 import Ember from 'ember';
 
+const ensureImage = (project) => (podcast) => project({
+  ...podcast,
+  imageURL: podcast.imageURL || `http://img.youtube.com/vi/${podcast.id}/hqdefault.jpg`
+});
+
+const splitDescriptionParagraphs = (project) => (podcast) => project({
+  ...podcast,
+  descriptionParagraphs: podcast.description.split('\n')
+});
+
+const groupsOfThree = (arr) =>
+  (arr.reduce(({ group, groups }, item, i) => {
+    if (!group || i % 3 === 0) {
+      group = [];
+      groups.push(group);
+    }
+    group.push(item);
+    return { groups, group };
+  }, { group: null, groups: [] })).groups;
+
 export default Ember.Route.extend({
   model() {
-    return this.modelFor('modern-web-podcast');
+    const model = this.modelFor('modern-web-podcast');
+    const podcasts = model.podcasts.map(ensureImage(splitDescriptionParagraphs(x => x)))
+    const groupedPodcasts = groupsOfThree(podcasts);
+    return { ...model, podcasts, groupedPodcasts };
   }
 });
