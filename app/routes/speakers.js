a3c00e4b7e7cd8146e12bf8bfbd66ac1693c7096
Tracy Lee <tracy@wingman.events>
2016-09-10 13:45:43 -0700

    save wip

---
+++ app/routes/speakers.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import speakers from 'modernweb/utils/speakers';
+
+export default Ember.Route.extend({
+  model: function() {
+		return speakers;
+	}
+});
--- app/utils/speakers.js
+++ app/utils/speakers.js
@@ -1,72 +1,528 @@
 export default [{
+	cardTitle: 'Matt Beale',
+	title: 'Ember Core Team',
+	image: 'img/speakers/matt-beale.png',
+	twitter: 'mixonic',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Luke Melia',
+	title: 'Co-Founder, Yapp Labs',
+	image: 'img/speakers/luke-melia.jpeg',
+	twitter: 'lukemelia',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Matt Podwysocki',
+	title: 'Principal Software Engineer, Microsoft',
+	image: 'img/speakers/matt-podwysocki.jpeg',
+	twitter: 'mattpodwysocki',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Charles Lowell',
+	title: 'Founder, The Frontside',
+	image: 'img/speakers/charles-lowell.jpg',
+	twitter: 'cowboyd',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Toran Billups',
+	title: 'Software Developer, Q2e Banking',
+	image: 'img/speakers/toran-billups.jpeg',
+	twitter: 'toranb',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Adam Bradley',
+	title: 'Ionic Core Team',
+	image: 'img/speakers/adam-bradley.jpeg',
+	twitter: 'adamdbradley',
+	types: ['event', 'podcast'],
+	current: true
+},
+{
+	cardTitle: 'Brandy Carney',
+	title: 'Ionic Core Team',
+	image: 'img/speakers/brandy-carney.jpg',
+	twitter: 'brandyscarney',
+	types: ['event', 'podcast'],
+	current: true
+},
+{
+	cardTitle: 'Robert DeLuca',
+	title: 'The Frontside',
+	image: 'img/speakers/robert-deluca.jpg',
+	twitter: 'robdel12',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'George Chapman',
+	title: 'Ember Developer',
+	image: 'img/speakers/george-chapman.jpg',
+	twitter: 'gnchapman',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Suz Hinton',
+	title: 'Developer',
+	image: 'img/speakers/suz-hinton.jpg',
+	twitter: 'noopkat',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Jamie White',
+	title: 'Web Developer, With Associates',
+	image: 'img/speakers/jamie-white.png',
+	twitter: 'jgwhite',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Ben Holmes',
+	title: 'Software Engineer, Zesty',
+	image: 'img/speakers/ben-holmes.jpeg',
+	twitter: 'binhums',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Bill Odom',
+	title: 'Angular Bootcamp',
+	image: 'img/speakers/bill-odom.jpg',
+	twitter: 'wnodom',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Matt Zabriskie',
+	title: 'Founder, React Rally',
+	image: 'img/speakers/matt-zabriskie.jpg',
+	twitter: 'mzabriskie',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Jamison Dance',
+	title: 'Founder, React Rally',
+	image: 'img/speakers/jamison-dance.jpeg',
+	twitter: 'jergason',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Ivan Vanderbyl',
+	title: 'Co-Founder, Flood.io',
+	image: 'img/speakers/ivan-vanderbyl.jpeg',
+	twitter: 'ivanderbyl',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Balint Erdi',
+	title: 'Author, Rock n Roll Ember',
+	image: 'img/speakers/balint-erdi.jpg',
+	twitter: 'baaz',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Ben Halpern',
+	title: 'Founder, The Practical Dev',
+	image: 'img/speakers/ben-halpern.jpg',
+	twitter: 'thepracticaldev',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Chase McCarthy',
+	title: 'EmberWeekend',
+	image: 'img/speakers/chase-mccarthy.png',
+	twitter: 'code0100fun',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Jonathan Jackson',
+	title: 'EmberWeekend',
+	image: 'img/speakers/jonathan-jackson.png',
+	twitter: 'rondale_sc',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Taras Mankovski',
+	title: 'EmberSherpa',
+	image: 'img/speakers/taras-mankovski.jpeg',
+	twitter: 'embersherpa',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Shai Reznik',
+	title: 'Founder, HiRez.io',
+	image: 'img/speakers/shai-reznik.jpg',
+	twitter: 'shai_reznik',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Hans Larsen',
+	title: 'Angular Core Team',
+	image: 'img/speakers/hans-larsen.jpg',
+	twitter: 'hanslatwork',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Sam Saccone',
+	title: 'Harbourmaster at Google',
+	image: 'img/speakers/sam-saccone.jpg',
+	twitter: 'samccone',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Zack Chapple',
+	title: 'Software Architect, CareerBuilder',
+	image: 'img/speakers/zackary-chapple.jpg',
+	twitter: 'zchapple',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Sarah Drasner',
+	title: 'Writer, CSS Tricks',
+	image: 'img/speakers/sarah-drasner.jpg',
+	twitter: 'sarah_edo',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Katherine Poulter',
+	title: '',
+	image: 'img/speakers/kat-poulter.jpg',
+	twitter: 'katpoulter',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Devon Lindsey',
+	title: 'Organizer, ReactJS SF',
+	image: 'img/speakers/devon-lindsey.jpg',
+	twitter: 'devonbl',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Tracy Lee',
+	title: 'Founder, Modern Web',
+	image: 'img/speakers/tracy-lee.jpg',
+	twitter: 'ladyleet',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Henry Zhu',
+	title: 'Babel Core Team',
+	image: 'img/speakers/henry-zhu.jpeg',
+	twitter: 'left_pad',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Raquel Moss',
+	title: 'Software Developer',
+	image: 'img/speakers/raquel-moss.jpeg',
+	twitter: 'raquelxmoss',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Michael Jackson',
+	title: 'Founder, React Training',
+	image: 'img/speakers/michael-jackson.jpeg',
+	twitter: 'mjackson',
+	types: ['podcast', 'event'],
+	current: true
+},
+{
+	cardTitle: 'Stephen Rivas Jr',
+	title: 'Stack Architect, Netflix',
+	image: 'img/speakers/stephen-rivas.jpeg',
+	twitter: 'sprjrx',
+	types: ['podcast', 'event'],
+	current: true
+},
+{
+	cardTitle: 'Andrew Joslin',
+	title: 'Software Engineer, Fact0ry',
+	image: 'img/speakers/andrew-joslin.jpg',
+	twitter: 'andrewtjoslin',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Dvid Silva',
+	title: 'Latinos in Tech',
+	image: 'img/speakers/dvid-silva.jpg',
+	twitter: 'dvidsilva',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Aysegul Yonet',
+	title: 'CTO AnnieCannons, GDE',
+	image: 'img/speakers/aysegul-yonet.jpg',
+	twitter: 'aysegulyonet',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Scott Moss',
+	title: 'Angular Class',
+	image: 'img/speakers/scott-moss.jpeg',
+	twitter: 'scotups',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Patrick J. Stapleton',
+	title: 'Angular Class',
+	image: 'img/speakers/patrick-stapleton.jpg',
+	twitter: 'gdi2290',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Mike "Broccoli" Brocchi',
+	title: 'Angular-CLI Core Team',
+	image: 'img/speakers/mike-brocchi.jpg',
+	twitter: 'brocco',
+	types: ['event', 'podcast'],
+	current: true
+},
+{
+	cardTitle: 'Jade Applegate',
+	title: 'Software Engineer, Fastly',
+	image: 'img/speakers/jade-applegate.jpg',
+	twitter: 'jadeapplegate',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Domenic Merenda',
+	title: 'Codefresh',
+	image: 'img/speakers/domenic-merenda.png',
+	twitter: 'domthewop',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Matt Billock',
+	title: 'Developer Advocate, Back&',
+	image: 'img/speakers/matt-billock.jpg',
+	twitter: 'backand_',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Devin Abbott',
+	title: 'Founder, Deco Software',
+	image: 'img/speakers/devin-abbott.jpg',
+	twitter: 'devinaabbott',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Jon Kuperman',
+	title: 'Web Developer, Twitter',
+	image: 'img/speakers/jon-kuperman.jpeg',
+	twitter: 'jkup',
+	types: ['event', 'podcast'],
+	current: true
+},
+{
+	cardTitle: 'Kent C. Dodds',
+	title: 'JavaScript Engineer, PayPal',
+	image: 'img/speakers/kent-dodds.jpg',
+	twitter: 'kentcdodds',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Marcus Hellberg',
+	title: 'Vaadin',
+	image: 'img/speakers/marcus-hellberg.jpg',
+	twitter: 'marcushellberg',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Arthur Evans',
+	title: 'Polymer Core Team',
+	image: 'img/speakers/arthur-evans.jpeg',
+	twitter: 'devdocdude',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Peter Burns',
+	title: 'Polymer Core Team',
+	image: 'img/speakers/peter-burns.jpg',
+	twitter: 'rictic',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Justin Fagnani',
+	title: 'Polymer Core Team',
+	image: 'img/speakers/justin-fagnani.jpeg',
+	twitter: 'justinfagnani',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Justin Ribeiro',
+	title: 'Google Developer Expert, Polymer',
+	image: 'img/speakers/justin-ribeiro.jpeg',
+	twitter: 'justinfagnani',
+	types: ['podcast'],
+	current: true
+},
+{
+	cardTitle: 'Rob Dodson',
+	title: 'Chrome Developer Advocate, Polymer Team',
+	image: 'img/speakers/rob-dodson.jpeg',
+	twitter: 'rob_dodson',
+	types: ['event', 'podcast'],
+	current: true
+},
+{
+	cardTitle: 'Kara Erickson',
+	title: 'Angular Core Team',
+	image: 'img/speakers/kara-erickson.jpg',
+	twitter: 'karaforthewin',
+	types: ['event'],
+	current: true
+},
+{
+	cardTitle: 'Victor Savkin',
+	title: 'Angular Core Team',
+	image: 'img/speakers/victor-savkin.jpg',
+	twitter: 'victorsavkin',
+	types: ['event', 'podcast'],
+	current: true
+},
+{
+	cardTitle: 'Alex Eagle',
+	title: 'Angular Core Team',
+	image: 'img/speakers/alex-eagle.jpg',
+	twitter: 'jakeherringbone',
+	types: ['event', 'podcast'],
+	current: true
+},
+{
 	cardTitle: 'Jeff Cross',
 	title: 'Angular Core Team',
 	image: 'img/speakers/jeff-cross.jpg',
+	twitter: 'jeffbcross',
+	types: ['event', 'podcast'],
 	current: true
 },
 {
 	cardTitle: 'James Kyle',
 	title: 'BabelJS Core Team',
 	image: 'img/speakers/james-kyle.jpg',
+	twitter: 'thejameskyle',
+	types: ['event', 'podcast'],
 	current: true
 },
 {
 	cardTitle: 'Ben Lesh',
-	title: 'Senior UI Engineer at Netflix',
-	image: 'img/speakers/ben-lesh.jpg',
+	title: 'RxJS 5 Lead',
+	image: 'img/speakers/ben-lesh-2.jpg',
+	twitter: 'benlesh',
+	types: ['event', 'podcast'],
 	current: true
 },
 {
 	cardTitle: 'Ariya Hidayat',
 	title: 'Creator PhantomJS, Esprima',
 	image: 'img/speakers/ariya-hidayat.jpg',
+	twitter: 'ariyahidayat',
+	types: ['event'],
 	current: true
 },
 {
 	cardTitle: 'Stefan Penner',
-	title: 'Ember Core Team, Hacker @Yahoo',
+	title: 'Ember Core Team',
 	image: 'img/speakers/stefan-penner.jpeg',
+	twitter: 'stefanpenner',
+	types: ['event'],
 	current: true
 },
 {
 	cardTitle: 'Jafar Husain',
 	title: 'Cross-Team Lead at Netflix, TC39 Member',
 	image: 'img/speakers/jafar-husain.jpg',
+	twitter: 'jhusain',
+	types: ['event', 'podcast'],
 	current: true
 },
 {
 	cardTitle: 'Jay Phelps',
 	title: 'Senior Front End Engineer at Netflix',
 	image: 'img/speakers/jay-phelps.jpeg',
+	twitter: '_jayphelps',
+	types: ['event', 'podcast'],
 	current: true
 },
 {
 	cardTitle: 'Godfrey Chan',
-	title: 'Rails Core Team, Ember at Tilde',
+	title: 'Rails Core Team, Ember Core Team',
 	image: 'img/speakers/godfrey-chan.jpg',
+	twitter: 'chancancode',
+	types: ['event'],
 	current: true
 },
 {
 	cardTitle: 'Yehuda Katz',
 	title: 'EmberJS Core Team',
 	image: 'img/speakers/yehuda-katz.jpg',
+	twitter: 'wycats',
+	types: ['event', 'podcast'],
 	current: true
 },
 {
 	cardTitle: 'Nathan Hammond',
-	title: 'Principal Web Developer at LinkedIn',
+	title: 'Ember-CLI Core Team',
 	image: 'img/speakers/nathan-hammond.jpg',
+	twitter: 'nathanhammond',
+	types: ['event'],
 	current: true
 },
 {
 	cardTitle: 'Jim Sproch',
 	title: 'React Core Team',
 	image: 'img/speakers/jim-sproch.jpg',
+	twitter: 'reactjs',
+	types: ['event'],
 	current: true
 },
 {
 	cardTitle: 'Jarrod Overson',
 	title: 'Developing Web Components',
 	image: 'img/speakers/jarrod-overson.jpg',
+	twitter: 'jsoverson',
+	types: ['event'],
 	current: true
 }];
