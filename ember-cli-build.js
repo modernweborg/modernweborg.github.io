eb2d22e05af958a70bbc2cfa975e64573c873d37
jorgelainfiesta <jorge.lainfiesta@gmail.com>
2016-11-24 13:55:11 -0600

    Update ember and clean up dependencies

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,12 +1,31 @@
+/*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var nodeSass = require('node-sass'); // loads the version in your package.json
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    // Add options here
     sassOptions: {
-      includePaths: ['bower_components/materialize/sass']
+      includePaths: [
+        'bower_components/materialize/sass'
+      ],
+      nodeSass: nodeSass // Workaround for ember-cli-sass bug https://github.com/aexmachina/ember-cli-sass/issues/117
     }
   });
+
+  // Use `app.import` to add additional libraries to the generated
+  // output files.
+  //
+  // If you need to use different assets in different
+  // environments, specify an object as the first parameter. That
+  // object's keys should be the environment name and the values
+  // should be the asset to use in that environment.
+  //
+  // If the library that you are including contains AMD or ES6
+  // modules that you would like to import into your application
+  // please specify an object with the list of modules as keys
+  // along with the exports of each module as its value.
 
   return app.toTree();
 };
